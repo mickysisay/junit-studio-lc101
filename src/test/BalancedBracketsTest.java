c53d5cdce65e19c2f9a873418da207ad0e13b047
Michael Sisay <ashenafimichael25@gmail.com>
2019-11-21 19:47:59 -0600

    done with studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -26,8 +26,12 @@ public class BalancedBracketsTest {
         assertTrue(test_Class.hasBalancedBrackets(""));
     }
     @Test
-    public void unbalancedBracketReturnsFalse(){
+    public void oneOpeningBracketReturnsFalse(){
         assertFalse(test_Class.hasBalancedBrackets("Launch[code"));
+    }
+    @Test
+    public void oneClosingBracketReturnsFalse(){
+        assertFalse(test_Class.hasBalancedBrackets("Launch]code"));
     }
     @Test
     public void balancedBracketReturnsTrue1(){
@@ -38,12 +42,20 @@ public class BalancedBracketsTest {
         assertTrue(test_Class.hasBalancedBrackets("[]LaunchCode"));
     }
     @Test
+    public void balancedBracketReturnsTrue3(){
+        assertTrue(test_Class.hasBalancedBrackets("[code]"));
+    }
+    @Test
     public void startWithClosingBracketReturnsFalse1(){
         assertFalse(test_Class.hasBalancedBrackets("]["));
     }
     @Test
     public void startWithClosingBracketReturnsFalse2(){
         assertFalse(test_Class.hasBalancedBrackets("Launch]Code["));
+    }
+    @Test
+    public void startWithClosingBracketReturnsFalse3(){
+        assertFalse(test_Class.hasBalancedBrackets("]Code["));
     }
     @Test
     public void noBracketReturnsTrue(){
