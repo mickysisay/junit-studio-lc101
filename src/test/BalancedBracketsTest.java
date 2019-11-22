5e2f95dc51dc50957f2988b8454678d767afd066
Michael Sisay <ashenafimichael25@gmail.com>
2019-11-21 19:00:59 -0600

    done 9 tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,56 @@
 package test;
 
+import main.BalancedBrackets;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
-
+     BalancedBrackets test_Class;
     //TODO: add tests here
     @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
+    @Before
+    public void createBalancedBracketsObject(){
+        test_Class = new BalancedBrackets();
+    }
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(test_Class.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void emptyStringReturnsTrue(){
+        assertTrue(test_Class.hasBalancedBrackets(""));
+    }
+    @Test
+    public void unbalancedBracketReturnsFalse(){
+        assertFalse(test_Class.hasBalancedBrackets("Launch[code"));
+    }
+    @Test
+    public void balancedBracketReturnsTrue1(){
+        assertTrue(test_Class.hasBalancedBrackets("Launch[code]"));
+    }
+    @Test
+    public void balancedBracketReturnsTrue2(){
+        assertTrue(test_Class.hasBalancedBrackets("[]LaunchCode"));
+    }
+    @Test
+    public void startWithClosingBracketReturnsFalse1(){
+        assertFalse(test_Class.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void startWithClosingBracketReturnsFalse2(){
+        assertFalse(test_Class.hasBalancedBrackets("Launch]Code["));
+    }
+    @Test
+    public void noBracketReturnsTrue(){
+        assertTrue(test_Class.hasBalancedBrackets("LaunchCode"));
+    }
+
+
 
 
 }
